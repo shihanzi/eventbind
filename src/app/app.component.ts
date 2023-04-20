68ed7dd6967fd8123578e7a42c5f01d0abe8d62f
Yasura Malshan <yasuramalshan@gmail.com>
2023-04-20 23:28:59 +0530

    component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private auth:AuthService) { }
+
   title = 'SiriPela';
+  sideBarOpen = true;
+  isLoggedIn = false;
 
+  ngOnInit(): void {
+    this.isLoggedIn = this.auth.isLoggedIn();
   }
-
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
+}
